824a6b9423ac9b25bc924e051370f5b52185eee5
GreenMeansGoBetting <133897269+GreenMeansGoBetting@users.noreply.github.com>
2026-02-26 17:20:34 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // CONFIG: paste your Supabase project URL + anon key here.
 // You get both from Supabase: Project Settings → API
-window.SUPABASE_URL = "PASTE_SUPABASE_URL_HERE";
-window.SUPABASE_ANON_KEY = "PASTE_SUPABASE_ANON_KEY_HERE";
+window.SUPABASE_URL = "https://mpvzicatxjzoaxtlieyw.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wdnppY2F0eGp6b2F4dGxpZXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMzEyOTcsImV4cCI6MjA4NzcwNzI5N30.QmpgY48F1_5OBeW0J8HQ_TDWfFy_rds-vnlDRtpFEqk";
