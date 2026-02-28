36c1e047e0b2ea161ab69e9e97ea0a57678b1c44
GreenMeansGoBetting <133897269+GreenMeansGoBetting@users.noreply.github.com>
2026-02-28 10:22:58 -0500

    Delete config.v9-8.js

--- config.v9-8.js
+++
@@ -1,3 +0,0 @@
-// CONFIG: Supabase project URL + anon key
-window.SUPABASE_URL = "https://mpvzicatxjzoaxtlieyw.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wdnppY2F0eGp6b2F4dGxpZXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMzEyOTcsImV4cCI6MjA4NzcwNzI5N30.QmpgY48F1_5OBeW0J8HQ_TDWfFy_rds-vnlDRtpFEqk";
